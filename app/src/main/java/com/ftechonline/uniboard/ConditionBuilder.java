48378c46d6d55d1cbfa6d5548817247529c5d775
Sreehari B S <sreeharibs95@gmail.com>
2018-03-17 14:40:17 +0530

    Initial Commit

---
+++ app/src/main/java/com/ftechonline/uniboard/ConditionBuilder.java
@@ -0,0 +1,8 @@
+package com.ftechonline.uniboard;
+
+/**
+ * Created by sreehari on 17/3/18.
+ */
+
+public class ConditionBuilder {
+}
---
+++ app/src/main/java/com/ftechonline/uniboard/Item.java
@@ -0,0 +1,14 @@
+package com.ftechonline.uniboard;
+
+/**
+ * Created by sreehari on 17/3/18.
+ */
+
+public class Item {
+
+    public Integer itemId;
+
+    public String itemName;
+
+
+}
